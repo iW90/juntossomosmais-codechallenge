f5f31b1c067d10655c23b93cc9af5774b8e35e9d
Dindinha <indynha.w@gmail.com>
2022-10-12 20:13:34 -0300

    Del old version

--- jsmclients/jsmclients.Core/Interfaces/IUseCaseAsync.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace jsmclients.Core.Interfaces
-{
-    public interface IUseCaseAsync<TRequest, TResponse>
-    {
-        Task<TResponse> ExecuteAsync(TRequest request);
-    }
-}
